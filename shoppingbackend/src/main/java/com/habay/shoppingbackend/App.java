848bd409d2de32775121a7ca06f92b7073a420b6
habay88 <biodun.banjoko@yahoo.com>
2019-12-10 12:28:46 +0100

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/habay/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.habay.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
