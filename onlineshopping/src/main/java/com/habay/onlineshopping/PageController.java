76fbeb7ca71fdb5a73e359411a2351cf12ea5bb9
habay88 <biodun.banjoko@yahoo.com>
2019-12-27 18:00:21 +0100

    Error opening linked pages

--- onlineshopping/src/main/java/com/habay/onlineshopping/PageController.java
+++ onlineshopping/src/main/java/com/habay/onlineshopping/PageController.java
@@ -12,12 +12,28 @@ public class PageController {
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-	mv.addObject("greeting", "Welcome to Spring Web MVC");
+	mv.addObject("title", "Home");
+	mv.addObject("userClickHome", true);
 	return mv;
-	/*@RequestMapping("/")
-	public String showpage() {
-		return "page";*/
-	}
 	
 	}
+/*
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title", "About Us");
+	mv.addObject("userClickAbout", true);
+	return mv;
+	
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title", "Contact Us");
+	mv.addObject("userClickContact", true);
+	return mv;*/
+	
+	}
+	
 
